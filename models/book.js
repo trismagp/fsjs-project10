a5b974262ad9e4a94c69daeb724cc7e77475ae69
Caroline Forslund <cf222ev@student.lnu.se>
2018-08-27 16:17:10 +0100

    Multiple Updates
    Models are updated with timestamp false. Ststic files are updated to load when app is running. Views are updated to work with the stylesheet.

--- models/book.js
+++ models/book.js
@@ -1,11 +1,18 @@
 'use strict';
+const dateFormat = require('dateformat');
+function getTimeStamp(){
+  return dateFormat(this.createdAt, "dddd, mmmm dS, yyyy, hh:MM TT");
+}
+
 module.exports = (sequelize, DataTypes) => {
   var Book = sequelize.define('Book', {
       title: DataTypes.STRING,
       author: DataTypes.STRING,
       genre: DataTypes.STRING,
       first_published: DataTypes.INTEGER
-  }, {});
+  }, {
+    timestamps: false
+  });
   Book.associate = function(models) {
     // associations can be defined here
   };
--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,54 @@
 const express = require('express');
 const router = express.Router();
 const Book = require('../models').Book;
-var Patron = require('../models').Patron;
-var Loan = require('../models').Loan;
+const Patron = require('../models').Patron;
+const Loan = require('../models').Loan;
 
 
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-// Books -----------------------------------------------------------------------
+// BOOKS -----------------------------------------------------------------------
+
+// GET All Books
 router.get('/books', function(req, res, next) {
   Book.findAll().then(function(books){
-    res.render('all_books', { books: books });
+    res.render('all_books', { books: books, title: "All Books" });
   });
 });
 
-router.get('/books/new', function(req, res, next) {
-  res.render('new_book');
+// GET Book by ID
+router.get('/books/:id', function(req, res, next){
+  Book.findById(req.params.id).then(function(book){
+    res.render('book_detail', { book: book, title: book.title });
+  });
 });
 
-// Patrons ---------------------------------------------------------------------
+// GET New Book Form
+router.get('/books/new', function(req, res, next) {
+  res.render('new_book', { title: "New Book" });
+});
+
+// POST New Book
+router.post('/books/new', function(req, res, next) {
+  Book.create(req.body).then(function(book){
+    res.redirect('/books/' + book.id);
+  });
+});
+
+// PATRONS ---------------------------------------------------------------------
 router.get('/patrons', function(req, res, next) {
-  res.render('all_patrons');
+  Patron.findAll().then(function(patrons){
+    res.render('all_patrons', { patrons: patrons, title: "All Patrons" });
+  });
+});
+
+// GET Patron by ID
+router.get('/patrons/:id', function(req, res, next){
+  Patron.findById(req.params.id).then(function(patron){
+    res.render('patron_detail', { patron: patron, title: patron.first_name + " " + patron.last_name });
+  });
 });
 
 router.get('/patrons/new', function(req, res, next) {
