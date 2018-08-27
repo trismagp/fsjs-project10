5d159dc6b20a9a9ec3ef00933f90475117470a1c
Caroline Forslund <cf222ev@student.lnu.se>
2018-08-27 21:34:34 +0100

    id added to book model

--- models/book.js
+++ models/book.js
@@ -6,6 +6,10 @@ function getTimeStamp(){
 
 module.exports = (sequelize, DataTypes) => {
   var Book = sequelize.define('Book', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
     title: DataTypes.STRING,
     author: DataTypes.STRING,
     genre: DataTypes.STRING,
